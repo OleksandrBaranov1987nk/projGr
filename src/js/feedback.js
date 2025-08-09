a840e03e98c0a379e4307cc30902995e0039f751
Oleksandr Baranov <sashok1987nk@gmail.com>
2025-08-09 10:06:28 +0200

    update

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -84,5 +84,6 @@ export async function initFeedback() {
   }
 }
 
+initFeedback();
 
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
 import { api } from './api.js';
 import { initFeedback } from './js/feedback.js';
 
-initFeedback();
